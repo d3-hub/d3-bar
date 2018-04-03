bafd54d523b1079941812ad842fbe6dea843f959
ckinmind <245160421@qq.com>
2018-04-03 17:01:04 +0800

    init

---
+++ config.js
@@ -0,0 +1,34 @@
+const demos = [
+  { title: '单柱图', path: '/demos/single-bar'},
+]
+
+docute.init({
+  landing: 'landing.html',
+  // announcement(route) {
+  //   const info = { type: 'warning' }
+  //   info.html = '<a style="margin-right:10px;" class="docute-button docute-button-mini docute-button-warning" href="https://github.com/egoist/donate" target="_blank">注意!</a> 图表还处于开发阶段，所以文档还在剧烈变动阶段.'
+  //   return info
+  // },
+  debug: true,
+  // home: 'https://raw.githubusercontent.com/egoist/docute/master/README.md',
+  repo: 'd3-hub/d3-bar',
+  tocVisibleDepth: 4,
+  nav: {
+    default: [
+      {title: '主页', path: '/home'},
+      {title: 'API文档', path: '/api'},
+      {title: '案例', path: '/case', type: 'dropdown', items: demos},
+    ]
+  },
+  plugins: [
+    docuteIframe(),
+    docsearch({
+      apiKey: '65360cf9a91d87cd455d2b286d0d89ee',
+      indexName: 'docute',
+      // tags: ['english', 'zh-Hans', 'zh-Hant', 'ja'],
+      tags: ['changelog', 'chart'],
+      url: 'https://docute.js.org'
+    }),
+    evanyou()
+  ]
+})
